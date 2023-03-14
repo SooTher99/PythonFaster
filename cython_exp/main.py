07b69f68587242f532735234192f99ced1dc533e
SooTher99 <tolikberdyev@gmail.com>
2023-03-14 13:54:03 +0300

    Оформил readme

--- cython_exp/main.py
+++ cython_exp/main.py
@@ -1,6 +1,6 @@
 import os
 import pyximport
-from timeit import timeit
+from timeit import Timer
 from random import shuffle
 
 pyximport.install(reload_support=True, build_dir=os.path.join(os.getcwd(), '.pyxbld'), language_level=3)
@@ -8,9 +8,10 @@ pyximport.install(reload_support=True, build_dir=os.path.join(os.getcwd(), '.pyx
 import insertion_sort
 
 if __name__ == "__main__":
-    random_list_of_nums = list(range(0, 10_000))
-    shuffle(random_list_of_nums)
+    setup = """
+random_list_of_nums = list(range(0, 10_000))
+shuffle(random_list_of_nums)
+"""
 
-    print(timeit('insertion_sort.insertion_sort(random_list_of_nums)', globals=globals(), number=1))
-
+    print(min(Timer('insertion_sort.insertion_sort(random_list_of_nums)', setup=setup, globals=globals()).repeat(7, 1000)))
 
